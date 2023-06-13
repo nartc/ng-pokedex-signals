c5a8dc82efc0f1e056e1115bf0ed3a0a29d79f7a
Chau Tran <chautran@Chaus-MacBook-Pro.local>
2023-06-12 22:55:45 -0500

    remove cache options

--- src/app/data-access-pokemons/pokemon-client.ts
+++ src/app/data-access-pokemons/pokemon-client.ts
@@ -1,20 +1,9 @@
-import { CacheOptions, buildKeyGenerator, defaultKeyGenerator } from 'axios-cache-interceptor';
+import { CacheOptions } from 'axios-cache-interceptor';
 import { PokemonClient } from 'pokenode-ts';
 import { createInjectionToken } from '../shared-utils/injection-token';
 
 function pokemonClientCacheOptionsFactory() {
-    return {
-        generateKey: buildKeyGenerator((request) => {
-            if (request.params) {
-                return {
-                    method: request.method,
-                    url: request.url,
-                    params: request.params.toString(),
-                };
-            }
-            return defaultKeyGenerator(request);
-        }),
-    } as CacheOptions;
+    return {} as CacheOptions;
 }
 
 export const [, providePokemonClientCacheOptions, POKEMON_CLIENT_CACHE_OPTIONS] = createInjectionToken(
